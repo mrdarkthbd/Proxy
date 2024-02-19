4227b45a4413662a51d4b625208d1ecc0acd6a87
mrdarkthbd <116535388+mrdarkthbd@users.noreply.github.com>
2024-02-19 18:15:16 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ else
             curl -s -L -o /usr/bin/proxy https://raw.githubusercontent.com/PhoenixxZ2023/proxy/main/proxy
             chmod +x /usr/bin/proxy
         } > /dev/null 2>&1
-        echo "Proxy instalado com sucesso."
+        echo "Proxy installed successfully."
     }
     
 # Instalar o proxy
@@ -21,7 +21,7 @@ fi
 
 # Desinstalar o proxy
 uninstall_proxy() {
-    echo -e "\nDesinstalando o proxy..."
+    echo -e "\nUninstalling the proxy..."
     
     # Encontra e remove todos os arquivos de serviço do proxy
     service_files=$(find /etc/systemd/system -name 'proxy-*.service')
@@ -42,7 +42,7 @@ uninstall_proxy() {
     # Remove o arquivo binário do proxy
     rm -f /usr/bin/proxy
     
-    echo "Proxy desinstalado com sucesso."
+    echo "Proxy uninstalled successfully."
 }
 
 # Mostrar portas em uso
@@ -50,25 +50,25 @@ show_ports_in_use() {
     local ports_in_use=$(systemctl list-units --all --plain --no-legend | grep -oE 'proxy-[0-9]+' | cut -d'-' -f2)
     if [ -n "$ports_in_use" ]; then
         ports_in_use=$(echo "$ports_in_use" | tr '\n' ' ')
-        echo -e "\033[1;34m║\033[1;32mEm uso:\033[1;33m $(printf '%-21s' "$ports_in_use")\033[1;34m║\033[0m"
+        echo -e "\033[1;34m║\033[1;32mIN use:\033[1;33m $(printf '%-21s' "$ports_in_use")\033[1;34m║\033[0m"
         echo -e "\033[1;34m║═════════════════════════════║\033[0m"
     fi
 }
 
 # Configurar e iniciar o serviço
 configure_and_start_service() {
-    read -p "QUE PORTA DESEJA ATIVAR? (--port): " PORT
-    read -p "Você quer usar HTTP(H) ou HTTPS(S)? [H/S]: " HTTP_OR_HTTPS
+    read -p "WHICH PORT DO YOU WANT TO ACTIVATE? (--port): " PORT
+    read -p "Do you want to use HTTP(H) ou HTTPS(S)? [H/S]: " HTTP_OR_HTTPS
     CERT_PATH="/root/cert.pem"  # Caminho padrão para o certificado
     RESPONSE=""
     
     if [[ $HTTP_OR_HTTPS == "H" || $HTTP_OR_HTTPS == "h" ]]; then
-        read -p "DIGITE STATUS DO PROXY DTUNNEL (--response): " RESPONSE
+        read -p "Add Your Proxy Banner (--response): " RESPONSE
     fi
     
-    read -p "Você quer usar apenas SSH (Y/N)? [Y/N]: " SSH_ONLY
-    read -p "Digite o tamanho do buffer (recomendado 32768) (--buffer-size): " BUFFER_SIZE
-    read -p "Digite o número de workers (recomendado 2500) (--workers): " WORKERS
+    read -p "You want to use SSH only (Y/N)? [Y/N]: " SSH_ONLY
+    read -p "Enter the buffer size (recommended 32768) (--buffer-size): " BUFFER_SIZE
+    read -p "type it the number of workers (recommended 2500) (--workers): " WORKERS
 
     # Defina as opções de comando
     OPTIONS="--port $PORT"
@@ -111,7 +111,7 @@ configure_and_start_service() {
 
 # Parar e remover serviços
 stop_and_remove_service() {
-    read -p "QUAL PORTA DESEJA REMOVER: " service_number
+    read -p "WHICH PORT DO YOU WANT TO REMOVE: " service_number
     
     # Parar o serviço
     systemctl stop proxy-$service_number
@@ -154,16 +154,16 @@ while true; do
 
     show_ports_in_use
     
-    echo -e "\033[1;34m║\033[1;36m[\033[1;32m01\033[1;36m] \033[1;32m• \033[1;31mABRIR PORTA           \033[1;34m║"
-    echo -e "\033[1;34m║\033[1;36m[\033[1;32m02\033[1;36m] \033[1;32m• \033[1;31mFECHAR PORTA          \033[1;34m║"
-    echo -e "\033[1;34m║\033[1;36m[\033[1;32m03\033[1;36m] \033[1;32m• \033[1;31mREINICIAR PORTA       \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m01\033[1;36m] \033[1;32m• \033[1;31mOPEN PORT           \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m02\033[1;36m] \033[1;32m• \033[1;31mCLOSE PORT          \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m03\033[1;36m] \033[1;32m• \033[1;31mRESET PORT       \033[1;34m║"
     echo -e "\033[1;34m║\033[1;36m[\033[1;32m04\033[1;36m] \033[1;32m• \033[1;31mMONITOR               \033[1;34m║"
-    echo -e "\033[1;34m║\033[1;36m[\033[1;32m05\033[1;36m] \033[1;32m• \033[1;31mREINSTALAR PORTA      \033[1;34m║"
-    echo -e "\033[1;34m║\033[1;36m[\033[1;32m06\033[1;36m] \033[1;32m• \033[1;31mDESINSTALAR PROXY     \033[1;34m║"
-    echo -e "\033[1;34m║\033[1;36m[\033[1;32m07\033[1;36m] \033[1;32m• \033[1;31mSAIR                  \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m05\033[1;36m] \033[1;32m• \033[1;31mINSTALLER PORT  \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m06\033[1;36m] \033[1;32m• \033[1;31mUNINSTALL PROXY     \033[1;34m║"
+    echo -e "\033[1;34m║\033[1;36m[\033[1;32m07\033[1;36m] \033[1;32m• \033[1;31mEXIT                  \033[1;34m║"
     echo -e "\033[1;34m╚═════════════════════════════╝\033[0m"
    
-    read -p "Escolha uma opção: " choice
+    read -p "Choose an option: " choice
 
     case $choice in
         1)
@@ -195,9 +195,9 @@ while true; do
             break
         ;;
         *)
-            echo "Opção inválida. Escolha uma opção válida."
+            echo "Invalid option. Choose a valid option."
         ;;
     esac
     
-    read -p "Pressione Enter para continuar..."
+    read -p "Press Enter to continue..."
 done
